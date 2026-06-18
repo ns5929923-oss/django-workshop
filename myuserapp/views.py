984de0e960fa9c5e6d6e54c1f4ae06130fe99aeb
Neha Singh <ns5929923@gmail.com>
2026-06-18 09:53:16 +0530

    Add files via upload

--- myuserapp/views.py
+++ myuserapp/views.py
@@ -16,6 +16,24 @@ def Shoppage(request):
 def contactprocess(request):
     a= int(request.POST['txt1'])
     b= int(request.POST['txt2'])
-    c= a + b
-    msg= "A value is",a," " "B vlaue is",b, " " "sum is ",c
-    return HttpResponse(msg)
+    c= int(request.POST['txt3'])
+    d= int(request.POST['txt4'])
+    e= int(request.POST['txt5'])
+    f= a+b+c+d+e
+    g=(f/500)*100
+
+    msg= "Subject1 marks",a,"Subject2 marks",b, "Subject3 marks",c, "Subject4 marks",d, "Subject5",e,"Sum of marks",f, "Percentage",g
+
+    result=""
+    if g>90:
+        result="Your garde A+"
+    elif (g>80 and g<90):
+        result="Your garde A"
+    elif(g>60 and g<50):
+        result="Your garde is B"
+    elif(g>50 and g<30):
+        result="Your grade is C"
+    else:
+        result= "You failed!"
+
+    return render(request, 'result.html',{'mya':a, 'myb':b, 'myc': c, 'myd':d, 'mye':e, 'myf':f, 'myg':g, 'result':result})
